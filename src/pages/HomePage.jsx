8cbc8f80661a2375b537d28f0022a4d15a70b564
wramadhan <wahyuramadhan0549@gmail.com>
2022-08-31 08:02:16 +0700

    menambahkan API MovieDB

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,27 @@
-import React, { Component } from 'react'
-import Card from '../components/Card'
-import NavBar from '../components/NavBar'
+import React, { Component } from 'react';
+import Card from '../components/Card';
+import NavBar from '../components/NavBar';
+import axios from 'axios';
 
 export default class HomePage extends Component {
   state = {
     image: '',
-    title: "",
+    title: [],
     rating:'',
   };
+
+componentDidMount =()=>{
+  axios.get('https://api.themoviedb.org/3/movie/now_playing?api_key=47182bd87a80c318c05c57ae7d42b9e2&language=en-US&page=1')
+  .then((response) => {
+    const { results } = response.data;
+    if (results){
+      console.log(results);
+    }
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+};
   render() {
     const listNews = [
         {
@@ -87,7 +101,7 @@ export default class HomePage extends Component {
       <>
       <NavBar/>
       <div className="px-6 py-6 bg-gradient-to-r from-cyan-500 to-blue-500 h-auto w-full">
-        <div className='grid grid-cols-6 gap-3'>
+        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 md:grid-cols-4 lg:grid-cols-6 gap-3'>
       {listNews.map((item) => {
           return <Card image={item.image} title={item.title} rating={item.rating} />;
         })}
