fdc17db74c55a2dc88a6cee971a7654d6d01d1a6
wramadhan <wahyuramadhan0549@gmail.com>
2022-08-31 22:15:27 +0700

    release 2 telah selesai

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,11 @@
-const Card = ({ image, title, rating }) => {
+const Card = ({ title, image }) => {
   return (
     <div className="bg-yellow-500/50 w-40 ml-5 h-auto py-2 rounded-lg grid place-content-center px-1">
       <div className="flex justify-center">
         <img
           src={
-            image
-              ? image
+            "https://image.tmdb.org/t/p/original/" + image
+              ? "https://image.tmdb.org/t/p/original/" + image
               : "https://via.placeholder.com/500x750.png/000000/FFFFFF/%20C/O%20https://placeholder.com/?text=No+image"
           }
           alt="Gambar"
@@ -13,9 +13,6 @@ const Card = ({ image, title, rating }) => {
         />
       </div>
       <h3 className="text-center font-bold text-white">{title}</h3>
-      <p className="text-center text-white font-light italic text-xs">
-        Rating {rating}
-      </p>
     </div>
   );
 };
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,106 +5,37 @@ import axios from 'axios';
 
 export default class HomePage extends Component {
   state = {
-    image: '',
     title: [],
-    rating:'',
   };
 
-componentDidMount =()=>{
-  axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1`)
-  .then((response) => {
-    const { results } = response.data;
-    if (results){
-      console.log(results);
-    }
-  })
-  .catch((err) => {
-    console.log(err);
-  })
-};
+  componentDidMount() {
+    const self = this;
+    axios
+      .get(`https://api.themoviedb.org/3/movie/now_playing?api_key=47182bd87a80c318c05c57ae7d42b9e2&language=en-US&page=1`)
+      .then(function (response) {
+        console.log(response.data.results);
+        self.setState({
+          title: response.data.results,
+        });
+      })
+      .catch(function (error) {
+        alert(error);
+      });
+  };
+
   render() {
-    const listNews = [
-        {
-          image:
-            "https://p4.wallpaperbetter.com/wallpaper/780/49/86/saitama-one-punch-man-anime-gloves-wallpaper-preview.jpg",
-          title: "One Punch Man S1",
-          rating:'8/10'
-        },
-        {
-            image:
-              "https://assets.pikiran-rakyat.com/crop/0x0:0x0/x/photo/2022/07/26/3198136442.jpeg",
-            title: "Itachi vs Saitama",
-            rating:'8/10'
-          },
-        {
-          image:
-            "https://w0.peakpx.com/wallpaper/759/390/HD-wallpaper-itachi-uchiha-kakashi-madara-mangekyo-sharingan-minato-sharingan.jpg",
-          title: "Itachi Berkhianat",
-          rating:'8/10'
-        },
-        {
-          image:
-            "https://m.media-amazon.com/images/M/MV5BNGYyNmI3M2YtNzYzZS00OTViLTkxYjAtZDIyZmE1Y2U1ZmQ2XkEyXkFqcGdeQXVyMTA4NjE0NjEy._V1_.jpg",
-          title: "Your Name",
-          rating:'8/10'
-        },
-        {
-          image:
-            "https://p4.wallpaperbetter.com/wallpaper/780/49/86/saitama-one-punch-man-anime-gloves-wallpaper-preview.jpg",
-          title: "One Punch Man S2",
-          rating:'8/10'
-        },
-        {
-            image:
-              "https://assets.pikiran-rakyat.com/crop/0x0:0x0/x/photo/2022/07/26/3198136442.jpeg",
-            title: "Itachi vs Saitama",
-            rating:'8/10'
-          },
-        {
-          image:
-            "https://w0.peakpx.com/wallpaper/759/390/HD-wallpaper-itachi-uchiha-kakashi-madara-mangekyo-sharingan-minato-sharingan.jpg",
-          title: "Itachi Berkhianat",
-          rating:'8/10'
-        },
-        {
-          image:
-            "https://m.media-amazon.com/images/M/MV5BNGYyNmI3M2YtNzYzZS00OTViLTkxYjAtZDIyZmE1Y2U1ZmQ2XkEyXkFqcGdeQXVyMTA4NjE0NjEy._V1_.jpg",
-          title: "Your Name",
-          rating:'8/10'
-        },
-        {
-          image:
-            "https://p4.wallpaperbetter.com/wallpaper/780/49/86/saitama-one-punch-man-anime-gloves-wallpaper-preview.jpg",
-          title: "One Punch Man S3",
-          rating:'8/10'
-        },
-        {
-            image:
-              "https://assets.pikiran-rakyat.com/crop/0x0:0x0/x/photo/2022/07/26/3198136442.jpeg",
-            title: "Itachi vs Saitama",
-            rating:'8/10'
-          },
-        {
-          image:
-            "https://w0.peakpx.com/wallpaper/759/390/HD-wallpaper-itachi-uchiha-kakashi-madara-mangekyo-sharingan-minato-sharingan.jpg",
-          title: "Itachi Berkhianat",
-          rating:'8/10'
-        },
-        {
-          image:
-            "https://m.media-amazon.com/images/M/MV5BNGYyNmI3M2YtNzYzZS00OTViLTkxYjAtZDIyZmE1Y2U1ZmQ2XkEyXkFqcGdeQXVyMTA4NjE0NjEy._V1_.jpg",
-          title: "Your Name",
-          rating:'8/10'
-        },
-        ]
     return (
       <>
       <NavBar/>
       <div className="px-6 py-6 bg-gradient-to-r from-cyan-500 to-blue-500 h-auto w-full">
         <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 md:grid-cols-4 lg:grid-cols-6 gap-3'>
-      {listNews.map((item) => {
-          return <Card image={item.image} title={item.title} rating={item.rating} />;
-        })}
+        {this.state.title.map((item, index) => {
+            return (
+              <div key={index}>
+              <Card title={item.title} image={item.poster_path}/>
+              </div>
+            );
+          })}
         </div>
       </div>
       </>
